fd22348e180938aae1a815d49c7eaa235c4e8906
Aaron Ai <yangkun.ayk@gmail.com>
2023-11-24 14:28:56 +0800

    Dev

--- src/main/java/org/example/ServerStartup.java
+++ src/main/java/org/example/ServerStartup.java
@@ -1,54 +1,91 @@
 package org.example;
 
-import java.util.HashMap;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.apache.rocketmq.broker.BrokerController;
+import org.apache.rocketmq.common.BrokerConfig;
 import org.apache.rocketmq.common.MQVersion;
+import org.apache.rocketmq.common.namesrv.NamesrvConfig;
+import org.apache.rocketmq.logging.InternalLogger;
+import org.apache.rocketmq.logging.InternalLoggerFactory;
 import org.apache.rocketmq.namesrv.NamesrvController;
+import org.apache.rocketmq.remoting.netty.NettyClientConfig;
+import org.apache.rocketmq.remoting.netty.NettyServerConfig;
 import org.apache.rocketmq.remoting.protocol.RemotingCommand;
+import org.apache.rocketmq.store.config.MessageStoreConfig;
 
 public class ServerStartup {
-    public final String nsAddr;
-    public final String broker1Addr;
-    final String broker1Name;
-    final String clusterName;
-    final NamesrvController namesrvController;
-    final BrokerController brokerControllerA;
-    final BrokerController brokerControllerB;
+    public static final InternalLogger logger = InternalLoggerFactory.getLogger(ServerStartup.class);
 
-    public ServerStartup() {
-        System.setProperty(
-            RemotingCommand.REMOTING_VERSION_KEY, Integer.toString(MQVersion.CURRENT_VERSION));
-        namesrvController = Utility.createAndStartNamesrv();
-        nsAddr = "localhost:" + namesrvController.getNettyServerConfig().getListenPort();
+    private static final String COMMIT_LOG_DIR_PREFIX = "commitlog";
+    private static final String LOCALHOST_STR = "127.0.0.1";
+    private static final String IP_PORT_SEPARATOR = ":";
 
-        brokerControllerA = Utility.createAndStartBrokerA(nsAddr);
-        clusterName = brokerControllerA.getBrokerConfig().getBrokerClusterName();
+    private static final int COMMIT_LOG_SIZE = 1024 * 1024 * 100;
+    private static final int INDEX_NUM = 1000;
+    private static final int NAMESRV_PORT = 9876;
 
-        broker1Name = brokerControllerA.getBrokerConfig().getBrokerName();
-        broker1Addr = "localhost:" + brokerControllerA.getNettyServerConfig().getListenPort();
-
-        brokerControllerB = Utility.createAndStartBrokerB(nsAddr);
+    private static String createTempDir() {
+        String path = null;
+        try {
+            File file = Files.createTempDirectory("rocketmq-benchmark").toFile();
+            path = file.getCanonicalPath();
+        } catch (IOException e) {
+            logger.warn("Error creating temporary directory.", e);
+        }
+        return path;
     }
 
-    public static void main(String[] args) {
-        final ServerStartup serverStartup = new ServerStartup();
-        Utility.initTopic("normal-topic-0", serverStartup.nsAddr, serverStartup.clusterName);
-        Utility.initTopic("normal-topic-1", serverStartup.nsAddr, serverStartup.clusterName);
-        Utility.initTopic("normal-topic-2", serverStartup.nsAddr, serverStartup.clusterName);
+    public static void startBroker(String namesrvAddr, String clusterName, String brokerName)
+        throws Exception {
+        BrokerConfig brokerConfig = new BrokerConfig();
+        MessageStoreConfig storeConfig = new MessageStoreConfig();
+        brokerConfig.setBrokerClusterName(clusterName);
+        brokerConfig.setBrokerName(brokerName);
+        brokerConfig.setBrokerIP1(LOCALHOST_STR);
+        brokerConfig.setNamesrvAddr(namesrvAddr);
+        brokerConfig.setEnablePropertyFilter(true);
+        String directory = createTempDir();
+        storeConfig.setStorePathRootDir(directory);
+        storeConfig.setStorePathCommitLog(directory + File.separator + COMMIT_LOG_DIR_PREFIX);
+        storeConfig.setMappedFileSizeCommitLog(COMMIT_LOG_SIZE);
+        storeConfig.setMaxIndexNum(INDEX_NUM);
+        storeConfig.setMaxHashSlotNum(INDEX_NUM * 4);
+        NettyServerConfig nettyServerConfig = new NettyServerConfig();
+        NettyClientConfig nettyClientConfig = new NettyClientConfig();
+        int port = PortUtils.findBrokerPort();
+        nettyServerConfig.setListenPort(port);
+        storeConfig.setHaListenPort(port + 1);
+        BrokerController brokerController = new BrokerController(brokerConfig, nettyServerConfig, nettyClientConfig,
+            storeConfig);
+        brokerController.initialize();
+        brokerController.start();
+    }
 
-        final HashMap<String, String> fifoAttr = new HashMap<>();
-        Utility.initTopic("fifo-topic-0", serverStartup.nsAddr, serverStartup.clusterName, fifoAttr);
-        Utility.initTopic("fifo-topic-1", serverStartup.nsAddr, serverStartup.clusterName, fifoAttr);
-        Utility.initTopic("fifo-topic-2", serverStartup.nsAddr, serverStartup.clusterName, fifoAttr);
+    public static void startNamesrv() throws Exception {
+        String baseDir = createTempDir();
+        Path kvConfigPath = Paths.get(baseDir, "namesrv", "kvConfig.json");
+        Path namesrvPath = Paths.get(baseDir, "namesrv", "namesrv.properties");
 
-        final HashMap<String, String> delayAttr = new HashMap<>();
-        Utility.initTopic("delay-topic-0", serverStartup.nsAddr, serverStartup.clusterName, delayAttr);
-        Utility.initTopic("delay-topic-1", serverStartup.nsAddr, serverStartup.clusterName, delayAttr);
-        Utility.initTopic("delay-topic-2", serverStartup.nsAddr, serverStartup.clusterName, delayAttr);
+        NamesrvConfig namesrvConfig = new NamesrvConfig();
+        NettyServerConfig nameServerNettyServerConfig = new NettyServerConfig();
 
-        final HashMap<String, String> transactionAttr = new HashMap<>();
-        Utility.initTopic("transaction-topic-0", serverStartup.nsAddr, serverStartup.clusterName, transactionAttr);
-        Utility.initTopic("transaction-topic-1", serverStartup.nsAddr, serverStartup.clusterName, transactionAttr);
-        Utility.initTopic("transaction-topic-2", serverStartup.nsAddr, serverStartup.clusterName, transactionAttr);
+        namesrvConfig.setKvConfigPath(kvConfigPath.toString());
+        namesrvConfig.setConfigStorePath(namesrvPath.toString());
+
+        nameServerNettyServerConfig.setListenPort(NAMESRV_PORT);
+        NamesrvController namesrvController =
+            new NamesrvController(namesrvConfig, nameServerNettyServerConfig);
+        namesrvController.initialize();
+        namesrvController.start();
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.setProperty(RemotingCommand.REMOTING_VERSION_KEY, Integer.toString(MQVersion.CURRENT_VERSION));
+        startNamesrv();
+        startBroker(LOCALHOST_STR + IP_PORT_SEPARATOR + NAMESRV_PORT, "benchmark-cluster", "benchmark-broker");
     }
 }
